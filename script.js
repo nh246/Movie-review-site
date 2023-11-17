25daac53694851ef72e1d05119dcd1623eac8ce0
nh246 <nazmul210130@diit.edu.bd>
2023-11-17 21:55:17 +0600

    making it responsive

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-          console.log(data.results)
+          // console.log(data.results)
         showMovies(data.results);
       });
   }
